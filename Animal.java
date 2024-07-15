c56983ca4f5dfb71e95369c5f7eb0451afa5d862
01fe22bca205 <01fe22bca205@kletch.ac.in>
2024-07-15 21:03:07 +0530

    program to interit

---
+++ Animal.java
@@ -0,0 +1,12 @@
+class Animal {
+    String name;
+    int age;
+
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+
+    void sleep() {
+        System.out.println("This animal sleeps.");
+    }
+}
